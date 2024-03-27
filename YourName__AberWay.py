6f26f5dbb4db9ace3444684fdf4f38b27530ab74
royhritaban <h.roy.23@abdn.ac.uk>
2024-03-27 15:28:49 +0000

    cost matrix added

--- YourName__AberWay.py
+++ YourName__AberWay.py
@@ -1,32 +1,43 @@
 #! /bin/python
 
-from aberway_background_code import create, update, main_loop
 import time
+
+from aberway_background_code import create, main_loop, update
 
 ColourFlip = False
 
-    
+
 screen, bg, lineList, nodeList = create(ColourFlip)
 update(None, screen, bg, lineList, nodeList, None, None, None, None, 0)
 
 
 # --- SET THESE VALUES TO AN EXAMPLE ---
 startPos = 0
-listOfNodesToPass = [10,11,14]
+listOfNodesToPass = [10, 11, 14]
 length = 676.75
 error = 0.06
 
+
 def path_update():
-    ListOfNodeId = [] #set the value of this to the nodes that your path takes
-    start = time.time_ns() # for timing your algorithm
+    ListOfNodeId = []  # set the value of this to the nodes that your path takes
+    start = time.time_ns()  # for timing your algorithm
     # ---------- ---------- YOUR CODE GOES HERE ---------- ----------
-
-
-
 
     # ---------- ---------- ---------- ---------- ---------- ---------- ----------
     end = time.time_ns()
-    update(ListOfNodeId, screen, bg, lineList, nodeList, startPos, listOfNodesToPass, length, error, end - start)
+    update(
+        ListOfNodeId,
+        screen,
+        bg,
+        lineList,
+        nodeList,
+        startPos,
+        listOfNodesToPass,
+        length,
+        error,
+        end - start,
+    )
+
 
 path_update()
 main_loop()
